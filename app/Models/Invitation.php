7a371c9bfe6ec899e8deb8643a0fcae6dedcc0dc
fityann <algifariyfityan@gmail.com>
2025-12-26 06:53:21 +0700

    push lagi

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'category',
+    ];
+
+    const CATEGORY_VIP = 'vip';
+    const CATEGORY_REGULAR = 'regular';
 }
